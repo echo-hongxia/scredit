a6d2dc5a72d9dd023ff6de8293ccfabce028f66c
echo <zhouhx03411@gmail.com>
2017-08-27 17:01:09 +0800

    adjust product index css style

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -36,7 +36,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   end
 
   version :thumb do
-    process resize_to_fit: [180, 180]
+    process resize_to_fit: [90, 60]
   end
 
   version :medium do
