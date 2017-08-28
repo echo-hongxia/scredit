d5018fcbb1797d1a37d88d5ad702914ca0b3ed0f
echo <zhouhx03411@gmail.com>
2017-08-28 10:43:47 +0800

    AddUserIdToCompanyprofiles migration error

--- db/migrate/20170825085215_add_user_id_to_companyprofiles.rb
+++ db/migrate/20170825085215_add_user_id_to_companyprofiles.rb
@@ -1,5 +1,5 @@
 class AddUserIdToCompanyprofiles < ActiveRecord::Migration[5.0]
   def change
-    add_column :Companyprofiles,:user_id,:integer
+    add_column :companyprofiles,:user_id,:integer
   end
 end
